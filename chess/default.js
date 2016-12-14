be66392c19789ec39e6335872d6797d4100de884
stevenlay <stlay@ucsd.edu>
2016-12-13 23:44:22 -0800

    added images

--- chess/default.js
+++ chess/default.js
@@ -1,5 +1,5 @@
 var game = new Chess();
-var board = new ChessBoard('chessBoard', 'start');
+var board = new ChessBoard('chessBoard','start');
 
 var handleMove = function(source, target) {
     var move = game.move({from: source, to:target});
