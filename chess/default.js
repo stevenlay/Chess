5f26962107502ac1293c5996f3ced2d9f4213441
stevenlay <stlay@ucsd.edu>
2016-12-13 22:03:22 -0800

    not displaying board

--- chess/default.js
+++ chess/default.js
@@ -1,11 +1,10 @@
-var startGame = function () {
-    var boardCFG = {
-        draggable: true,
-        position: 'start',
-        onDrop: handleMove,
-    };
+var game = new Chess();
+var board = new ChessBoard('chessBoard', 'start');
 
-    game = new Chess();
-    var board = new ChessBoard('board');
-
+var handleMove = function(source, target) {
+    var move = game.move({from: source, to:target});
 }
+
+
+$('#startBtn').on('click', chessBoard.start);
+$('#clearBtn').on('click', chessBoard.clear);
