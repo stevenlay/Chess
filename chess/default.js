f88b739df8e79b2778a379d9c5945f17d208b248
stevenlay <stlay@ucsd.edu>
2016-12-15 16:04:21 -0800

    partial: gray square for movements

--- chess/default.js
+++ chess/default.js
@@ -3,14 +3,40 @@ var cfg = {
     draggable: true,
     position: 'start',
     onDrop: handleMove,
+    onMouseoverSquare: onMouseoutSquare,
+    onSnapEnd: onSnapEnd
 };
 
 
 var game = new Chess();
 var board = new ChessBoard('board', cfg);
 
+
+var removeGreySquares = function() {
+  $('#board .square-55d63').css('background', '');
+};
+
+var greySquare = function(square) {
+    var squareEl = $('#board .square-' + square);
+
+    var background = '#a9a9a9';
+    if (squareEl.hasClass('black-3c85d')) {
+        background = '#696969'
+    }
+
+    squareEl.css('background', background);
+};
+
 var handleMove = function(source, target) {
     var move = game.move({from: source, to:target});
+};
+
+var onMouseoutSquare = function(square, piece) {
+    removeGreySquares();
+};
+
+var onSnapEnd = function() {
+    board.position(game.fen());
 };
 
 $('#startBtn').on('click', board.start);
