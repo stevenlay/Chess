e9ed1d570576c70f79c67262a5426e85e1158076
stevenlay <stlay@ucsd.edu>
2016-12-18 23:54:34 -0800

    default.js syntax

--- chess/default.js
+++ chess/default.js
@@ -1,5 +1,4 @@
-var game = new Chess();
-
+var board, game = new Chess();
 
 var removeGreySquares = function() {
   $('#board .square-55d63').css('background', '');
@@ -9,7 +8,7 @@ var greySquare = function(square) {
     var squareEl = $('#board .square-' + square);
 
     var background = '#a9a9a9';
-    if (squareEl.hasClass('black-3c85d')) {
+    if (squareEl.hasClass('black-3c85d')=== true) {
         background = '#696969';
     }
 
@@ -18,7 +17,7 @@ var greySquare = function(square) {
 
 var onDragStart = function(source, piece) {
     // if game is over or not your turn (black or white side)
-    if (game.game_over() || (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+    if (game.game_over() === true|| (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
         (game.turn() === 'b' && piece.search(/^w/) !== -1)){
             return false;
     }
@@ -28,8 +27,12 @@ var onDrop = function(source, target) {
     removeGreySquares();
 
     //snapback if not a valid move
-    var move = handleMove(source,target);
-    if(!move) {
+    var move = game.move({
+        from: source,
+        to: target,
+        promotion: 'q' // NOTE: always promote to a queen for example simplicity
+    });
+    if(!move === null) {
         return 'snapback';
     }
 };
@@ -73,13 +76,13 @@ var cfg = {
     position: 'start',
     onDrop: onDrop,
     onMouseoutSquare: onMouseoutSquare,
-    onSnapEnd: onSnapEnd,
     onDragStart: onDragStart,
-    onMouseoverSquare: onMouseoverSquare
+    onMouseoverSquare: onMouseoverSquare,
+    onSnapEnd: onSnapEnd
 };
 
 
-var board = new ChessBoard('board', cfg);
+board = new ChessBoard('board', cfg);
 
 $('#startBtn').on('click', board.start);
 $('#clearBtn').on('click', board.clear);
