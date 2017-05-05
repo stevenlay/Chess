6aaa2dc9c224f12637b19f6fb2cd61be91bc5903
stevenlay <stlay@ucsd.edu>
2017-05-05 16:56:51 -0700

    fixed ports

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 app.use(express.static('chess'));
 var http = require('http').Server(app);
-var port = 3000;
+var port = process.env.port || 3000;
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/chess/default.html');
